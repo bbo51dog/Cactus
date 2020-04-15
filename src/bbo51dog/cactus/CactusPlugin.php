24c91f7abc817346fcaf7d24a8bc3a1474b4257d
bbo51dog <bbo51dog@gmail.com>
2020-04-15 22:37:46 +0900

    Register commands

--- src/bbo51dog/cactus/CactusPlugin.php
+++ src/bbo51dog/cactus/CactusPlugin.php
@@ -2,6 +2,7 @@
 
 namespace bbo51dog\cactus;
 
+use bbo51dog\cactus\command\CactusCommand;
 use pocketmine\plugin\PluginBase;
 
 class CactusPlugin extends PluginBase{
@@ -17,5 +18,13 @@ class CactusPlugin extends PluginBase{
         }
         fclose($file);
         $wordList = new WordList(json_decode($json, true));
+        $commandMap = $this->getServer()->getCommandMap()->registerAll('cactus', [
+            new CactusCommand($wordList, WordList::KEY_BBO),
+            new CactusCommand($wordList, WordList::KEY_LAZYPERSON),
+            new CactusCommand($wordList, WordList::KEY_REE),
+            new CactusCommand($wordList, WordList::KEY_SAISANA),
+            new CactusCommand($wordList, WordList::KEY_TAKUMATUTI),
+            new CactusCommand($wordList, WordList::KEY_YURISI),
+        ]);
     }
 }
---
+++ src/bbo51dog/cactus/command/CactusCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace bbo51dog\cactus\command;
+
+use bbo51dog\cactus\WordList;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class CactusCommand extends Command{
+
+    /** @var string */
+    private $wordKey;
+    
+    /** @var WordList */
+    private $wordList;
+    
+    public function __construct(WordList $wordList, string $wordKey){
+        $this->wordList = $wordList;
+        $this->wordKey = $wordKey;
+        parent::__construct(strtolower($this->wordKey), "{$this->wordKey}の名言をランダムに発言します");
+    }
+    
+    public function execute(CommandSender $sender, $commandLabel, string $args){
+        
+    }
+}
